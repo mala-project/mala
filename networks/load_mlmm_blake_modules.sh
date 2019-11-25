90a89aae09370a18f0fca2351d846523efe3c22d
J. Austin Ellis <johelli@sandia.gov>
2019-11-25 15:45:31 -0700

    Add README for networks, scripts for setting up env, and ldos data parsers

---
+++ networks/load_mlmm_blake_modules.sh
@@ -0,0 +1,11 @@
+
+# Modules for MLMM
+
+module rm gcc
+module rm python
+module rm openmpi
+
+module load python/3.7.3
+module load openmpi/2.1.2/gcc/7.2.0
+
+module list
---
+++ networks/setup_python_env.sh
@@ -0,0 +1,45 @@
+
+#### Sets up python env for the MLMM project
+
+# Run on machines/testbeds with no root privs
+
+#pip3 install --upgrade pip --user
+
+source load_mlmm_blake_modules.sh
+
+pip3 install --user virtualenv
+
+python3 -m venv mlmm_env
+
+source mlmm_env/bin/activate
+
+pip3 install numpy 
+pip3 install scipy 
+pip3 install matplotlib
+pip3 install seaborn
+pip3 install tensorboard
+
+# Parallel training
+pip3 install horovod
+
+# Use PyTorch
+pip3 install torch
+pip3 install torchvision
+
+# Atomic Simulation Environment
+pip3 install ase
+
+
+
+# Use TF
+#pip3 install keras=2.2.5
+#pip3 install tensorflow==1.14.0
+#pip3 install tensorflow_datasets 
+#pip3 install tensorboard 
+
+# Not Tested
+#pip3 install tensorflow-gpu
+#pip3 install tensorflow-gpu==2.0.0-rc0
+
+pip3 list
+
