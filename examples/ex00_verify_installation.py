419831fac89a6e187c8a38881f0bb8062532399a
Lenz Fiedler <l.fiedler@hzdr.de>
2020-12-15 22:38:31 +0100

    Reintroduced Descriptors and targets to ex00

--- examples/ex00_verify_installation.py
+++ examples/ex00_verify_installation.py
@@ -1,8 +1,6 @@
 from mldft.common.parameters import Parameters
 from mldft.datahandling.handler_interface import HandlerInterface
 from mldft.network.network import Network
-
-# For the next two we only test the import to see whether or not LAMMPS is installed.
 from mldft.descriptors.descriptor_interface import DescriptorInterface
 from mldft.targets.target_interface import TargetInterface
 
@@ -15,7 +13,9 @@ print("Welcome to ML-DFT@CASUS.")
 print("Running ex0_verify_installation.py")
 
 test_parameters = Parameters()
-test_handler = HandlerInterface(test_parameters)
+test_descriptors = DescriptorInterface(test_parameters)
+test_targets = TargetInterface(test_parameters)
+test_handler = HandlerInterface(test_parameters, descriptor_calculator=test_descriptors, target_parser=test_targets)
 test_network = Network(test_parameters)
 
 print("Successfully ran ex0_verify_installation.py.")
