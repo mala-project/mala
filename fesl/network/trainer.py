05a72b37f8c06c60c33c34498ee1a416be69d72e
Lenz Fiedler <l.fiedler@hzdr.de>
2021-01-26 16:26:28 +0100

    Added fix for the validation function

--- fesl/network/trainer.py
+++ fesl/network/trainer.py
@@ -115,18 +115,14 @@ class Trainer:
         self.optimizer.zero_grad()
         return loss.item()
 
-    # FIXME: This seems inefficient.
     def validate_network(self, network, vdl):
         network.eval()
-        accuracies = []
-        validation_loss = 0
+        validation_loss = []
         with torch.no_grad():
             for x, y in vdl:
                 if self.use_gpu:
                     x = x.to('cuda')
                     y = y.to('cuda')
                 prediction = network(x)
-                validation_loss += network.calculate_loss(prediction, y).item()
-                # accuracies.append(network.classification_accuracy(prediction, y))
-            # validation_accuracy = np.mean(accuracies)
-        return validation_loss
+                validation_loss.append(network.calculate_loss(prediction, y).item())
+        return np.mean(validation_loss)
