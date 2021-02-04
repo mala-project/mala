41b08bdd2777b501b7df56013f09c9be50669314
Lenz Fiedler <l.fiedler@hzdr.de>
2021-02-04 12:00:38 +0100

    Added installation prerequisites to .yaml and test for OA Package to installation verifier

--- examples/ex00_verify_installation.py
+++ examples/ex00_verify_installation.py
@@ -2,6 +2,7 @@ from fesl.common.parameters import Parameters
 from fesl.common.parameters import printout
 from fesl.datahandling.handler_interface import HandlerInterface
 from fesl.network.network import Network
+from fesl.network.hyperparameter_interface import HyperparameterInterface
 from fesl.descriptors.descriptor_interface import DescriptorInterface
 from fesl.targets.target_interface import TargetInterface
 
@@ -18,6 +19,7 @@ test_descriptors = DescriptorInterface(test_parameters)
 test_targets = TargetInterface(test_parameters)
 test_handler = HandlerInterface(test_parameters, descriptor_calculator=test_descriptors, target_parser=test_targets)
 test_network = Network(test_parameters)
+test_hpoptimizer = HyperparameterInterface(test_parameters)
 
 printout("Successfully ran ex00_verify_installation.py.")
 printout("Congratulations, your installation seems to work!")
