2436ba226f70aaabf7abf3870fe3041852f5511d
Lenz Fiedler <l.fiedler@hzdr.de>
2021-02-09 11:45:42 +0100

    Added the DataConverter class, completely untested

--- fesl/datahandling/data_converter.py
+++ fesl/datahandling/data_converter.py
@@ -1,32 +1,31 @@
-from .handler_base import HandlerBase
-import numpy as np
 from fesl.common.parameters import printout
+from fesl.descriptors.descriptor_interface import DescriptorInterface
+from fesl.targets.target_interface import TargetInterface
+import numpy as np
 
-
-class HandlerQEoutCube(HandlerBase):
-    """Collects data from a QuantumEspresso calculation.
-    Input data is read by reading the QuantumEspresso outfile.
-    Output data is read by parsing multiple *.cube files.
+class DataConverter:
+    """
+    This class can use a couple of raw snapshots (i.e. direct outputs of QE) and transform them into snapshots
+    the FESL DataHandler class can use easily.
     """
 
-    def __init__(self, p, input_data_scaler, output_data_scaler, descriptor_calculator, target_parser):
-        super(HandlerQEoutCube, self).__init__(p, input_data_scaler, output_data_scaler, descriptor_calculator,
-                                               target_parser)
+    def __init__(self, p, descriptor_calculator=None, target_calculator=None):
+        self.target_calculator = target_calculator
+        if self.target_calculator is None:
+            self.target_calculator = TargetInterface(p)
 
-    def add_snapshot(self, qe_out_file, qe_out_directory, cube_naming_scheme, cube_directory,
-                     input_units=None, output_units="1/eV"):
-        """Adds a snapshot to data handler. For this type of data,
-        a QuantumEspresso outfile, an outfile from the LDOS calculation and
-        a directory containing the cube files"""
-        self.parameters.snapshot_directories_list.append(
-            [qe_out_file, qe_out_directory, cube_naming_scheme, cube_directory])
+        self.descriptor_calculator = descriptor_calculator
+        if self.descriptor_calculator is None:
+            self.descriptor_calculator = DescriptorInterface(p)
 
-    def load_data(self):
-        """Loads data and transforms it into descriptor / target data on the grid.
-        At the end of this function we have two pytorch tensors holding the input and the output data."""
+        self.snapshots_to_convert = []
 
-        # Load from every snapshot directory.
-        for snapshot in self.parameters.snapshot_directories_list:
+    def add_snapshot_qeout_cube(self, qe_out_file, qe_out_directory, cube_naming_scheme, cube_directory):
+        self.snapshots_to_convert.append([qe_out_file, qe_out_directory, cube_naming_scheme, cube_directory])
+
+    def convert_snapshots(self, save_path="./", naming_scheme="ELEM_snapshot*"):
+        i = 0
+        for snapshot in self.snapshots_to_convert:
             ##############
             # Input data.
             # Calculate the SNAP descriptors from the QE calculation.
@@ -40,26 +39,18 @@ class HandlerQEoutCube(HandlerBase):
             # Read the LDOS data.
             ##############
 
-            printout("Reading targets for snapshot ", snapshot[2], "at ", snapshot[3])
-            self.target_parser.read_from_cube(snapshot[2], snapshot[3])
+            printout("Reading targets from ", snapshot[2], "at ", snapshot[3])
+            tmp_input = self.target_calculator.read_from_cube(snapshot[2], snapshot[3])
+
 
             # Here, raw_input only contains the file name given by ASE and the dimensions of the grd.
             # These are the input parameters we need for LAMMPS.
-            # self.raw_input.append(self.get_atoms_from_qe(self.parameters.directory+sub))
+            printout("Calculating descriptors for ", snapshot[0], "at ", snapshot[1])
+            tmp_output = self.descriptor_calculator.calculate_snap(snapshot[1]+snapshot[0], snapshot[1])
 
-    # def prepare_data(self):
-    #     """Cuts input/output data into training, validation and test set.
-    #     then adds the training, validation and test data to pytorch datahandlers
-    #     to be used by the network."""
-
-    def get_input_dimension(self):
-        if self.descriptor_calculator.fingerprint_length > 0:
-            return self.descriptor_calculator.fingerprint_length
-        else:
-            raise Exception("No descriptors were calculated, cannot give input dimension.")
-
-    def get_output_dimension(self):
-        if self.parameters.ldos_gridsize > 0:
-            return self.target_parser.target_length
-        else:
-            raise Exception("No targets were read, cannot give output dimension.")
+            snapshot_name = naming_scheme
+            snapshot_name = snapshot_name.replace("*", str(i))
+            printout("Saving ", i, "at ", save_path)
+            np.save(snapshot_name+".in.npy", tmp_input)
+            np.save(snapshot_name+".out.npy", tmp_output)
+            i += 1
