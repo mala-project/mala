850c71bb7fadba3a4f34f6f564ef3122a840f03e
Lenz Fiedler <l.fiedler@hzdr.de>
2021-03-05 17:21:47 +0100

    GPU test suceeds on CUDA system and fails on CPU only machine

--- test/basic_gpu_test.py
+++ test/basic_gpu_test.py
@@ -8,7 +8,6 @@ test_checkpoint_name = "test"
 
 
 def test_run(use_gpu):
-    starttime = time.time()
     ####################
     # PARAMETERS
     # All parameters are handled from a central parameters class that
@@ -21,7 +20,8 @@ def test_run(use_gpu):
     # done by providing a list containing entries of the form
     # "tr", "va" and "te".
     test_parameters.data.data_splitting_type = "by_snapshot"
-    test_parameters.data.data_splitting_snapshots = ["tr", "va", "te"]
+    test_parameters.data.data_splitting_snapshots = ["tr", "tr", "tr", "tr",
+                                                     "tr", "tr", "va", "te"]
 
     # Specify the data scaling.
     test_parameters.data.input_rescaling_type = "feature-wise-standard"
@@ -31,7 +31,7 @@ def test_run(use_gpu):
     test_parameters.network.layer_activations = ["ReLU"]
 
     # Specify the training parameters.
-    test_parameters.running.max_number_epochs = 200
+    test_parameters.running.max_number_epochs = 100
     test_parameters.running.mini_batch_size = 40
     test_parameters.running.learning_rate = 0.00001
     test_parameters.running.trainingtype = "Adam"
@@ -47,9 +47,10 @@ def test_run(use_gpu):
     data_handler = fesl.DataHandler(test_parameters)
 
     # Add a snapshot we want to use in to the list.
-    data_handler.add_snapshot("Al_debug_2k_nr0.in.npy", data_path,
-                              "Al_debug_2k_nr0.out.npy", data_path,
-                              output_units="1/Ry")
+    for i in range(0, 6):
+        data_handler.add_snapshot("Al_debug_2k_nr0.in.npy", data_path,
+                                  "Al_debug_2k_nr0.out.npy", data_path,
+                                  output_units="1/Ry")
     data_handler.add_snapshot("Al_debug_2k_nr1.in.npy", data_path,
                               "Al_debug_2k_nr1.out.npy", data_path,
                               output_units="1/Ry")
@@ -73,12 +74,13 @@ def test_run(use_gpu):
     # Setup network and trainer.
     test_network = fesl.Network(test_parameters)
     test_trainer = fesl.Trainer(test_parameters, test_network, data_handler)
+    starttime = time.time()
     test_trainer.train_network()
 
-    return test_trainer.final_test_loss, time.time()-starttime
+    return test_trainer.final_test_loss, time.time() - starttime
 
 
-def perform_gpu_test(accuracy = 1e-13):
+def perform_gpu_test(accuracy=1e-7, performance_improvement=1.2):
     cpu_result = test_run(False)
     gpu_result = test_run(True)
 
@@ -87,10 +89,13 @@ def perform_gpu_test(accuracy = 1e-13):
         printout(cpu_result[0], gpu_result[0])
         return False
 
-    # We should at LEAST get twice the performance out of a GPU.
-    if gpu_result[1] > cpu_result[1] / 2:
+    # This test is not that well suited for GPU performance
+    # but we should at least see some kind of speed up.
+    printout(cpu_result[1], gpu_result[1])
+    if gpu_result[1] > cpu_result[1] / performance_improvement:
         printout(cpu_result[1], gpu_result[1])
         return False
+    return True
 
 
 if __name__ == "__main__":
