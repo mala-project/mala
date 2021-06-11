0d63f765d42e1d20e06f053efe60178ffd031845
Lenz Fiedler <l.fiedler@hzdr.de>
2021-06-11 11:12:38 +0200

    Deleted total energy module

--- ml-dft-sandia/total_energy_module/blake_qe_env_gnu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "In addition to sourcing this script, source mlmm-ldrd-data/networks/environment/load_mlmm_blake_modules.sh"
-
-export FCFLAGS=-fPIC
-export FFLAGS=-fPIC
-export CFLAGS=-fPIC
-export CPPFLAGS=-fPIC
-
