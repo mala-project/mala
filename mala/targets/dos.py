fc43630b4ff714a42e63038fd7e6b8f2e21b0a7a
Lenz Fiedler <l.fiedler@hzdr.de>
2021-06-25 16:58:11 +0200

    Fixing history (2): These routines were reimplemented based on code by Normand Modine
    Co-authored-by: Normand Modine <namodin@sandia.gov>

--- mala/targets/dos.py
+++ mala/targets/dos.py
@@ -341,6 +341,60 @@ class DOS(TargetBase):
                                             fermi_energy_eV, temperature_K,
                                             integration_method)
 
+    def get_self_consistent_fermi_energy_ev(self, dos_data,
+                                            temperature_K=None,
+                                            integration_method="analytical",
+                                            shift_energy_grid=True):
+        """
+        Calculate the self-consistent Fermi energy.
+
+        "Self-consistent" does not mean self-consistent in the DFT sense,
+        but rather the Fermi energy, for which DOS integration reproduces
+        the exact number of electrons. The term "self-consistent" stems
+        from how this quantity is calculated.
+
+        Parameters
+        ----------
+        dos_data : numpy.array
+            DOS data with dimension [energygrid].
+
+        temperature_K : float
+            Temperature in K.
+
+        integration_method : string
+            Integration method to be used. Currently supported:
+
+                - "trapz" for trapezoid method
+                - "simps" for Simpson method.
+                - "analytical" for analytical integration. Recommended.
+
+        shift_energy_grid : bool
+            When using the analytical integration, one has to shift the energy
+            grid by setting this parameter to True. Elsewise keep on False.
+
+        Returns
+        -------
+        fermi_energy_self_consistent : float
+            E_F in eV.
+        """
+        # Parse the parameters.
+        if temperature_K is None:
+            temperature_K = self.temperature_K
+        if shift_energy_grid and integration_method == "analytical":
+            energy_grid = self.get_energy_grid(shift_energy_grid=True)
+        else:
+            energy_grid = self.get_energy_grid()
+        fermi_energy_sc = toms748(lambda fermi_sc:
+                                  (self.
+                                   __number_of_electrons_from_dos
+                                   (dos_data, energy_grid,
+                                    fermi_sc, temperature_K,
+                                    integration_method)
+                                   - self.number_of_electrons),
+                                  a=energy_grid[0],
+                                  b=energy_grid[-1])
+        return fermi_energy_sc
+
     def get_density_of_states(self, dos_data):
         """Get the density of states."""
         return dos_data
@@ -380,3 +434,116 @@ class DOS(TargetBase):
 
         return return_dos_object
 
+    @staticmethod
+    def __number_of_electrons_from_dos(dos_data, energy_grid, fermi_energy_eV,
+                                       temperature_K, integration_method):
+        """Calculate the number of electrons from DOS data."""
+        # Calculate the energy levels and the Fermi function.
+
+        fermi_vals = fermi_function(energy_grid, fermi_energy_eV,
+                                    temperature_K)
+        # Calculate the number of electrons.
+        if integration_method == "trapz":
+            number_of_electrons = integrate.trapz(dos_data * fermi_vals,
+                                                  energy_grid, axis=-1)
+        elif integration_method == "simps":
+            number_of_electrons = integrate.simps(dos_data * fermi_vals,
+                                                  energy_grid, axis=-1)
+        elif integration_method == "quad":
+            dos_pointer = interpolate.interp1d(energy_grid, dos_data)
+            number_of_electrons, abserr = integrate.quad(
+                lambda e: dos_pointer(e) * fermi_function(e, fermi_energy_eV,
+                                                          temperature_K),
+                energy_grid[0], energy_grid[-1], limit=500,
+                points=fermi_energy_eV)
+        elif integration_method == "analytical":
+            number_of_electrons = analytical_integration(dos_data, "F0", "F1",
+                                                         fermi_energy_eV,
+                                                         energy_grid,
+                                                         temperature_K)
+        else:
+            raise Exception("Unknown integration method.")
+
+        return number_of_electrons
+
+    @staticmethod
+    def __band_energy_from_dos(dos_data, energy_grid,
+                               fermi_energy_eV,
+                               temperature_K, integration_method):
+        """Calculate the band energy from DOS data."""
+        # Calculate the energy levels and the Fermi function.
+        fermi_vals = fermi_function(energy_grid, fermi_energy_eV,
+                                    temperature_K)
+
+        # Calculate the band energy.
+        if integration_method == "trapz":
+            band_energy = integrate.trapz(dos_data * (energy_grid *
+                                                      fermi_vals),
+                                          energy_grid, axis=-1)
+        elif integration_method == "simps":
+            band_energy = integrate.simps(dos_data * (energy_grid *
+                                                      fermi_vals),
+                                          energy_grid, axis=-1)
+        elif integration_method == "quad":
+            dos_pointer = interpolate.interp1d(energy_grid, dos_data)
+            band_energy, abserr = integrate.quad(
+                lambda e: dos_pointer(e) * e * fermi_function(e,
+                                                              fermi_energy_eV,
+                                                              temperature_K),
+                energy_grid[0], energy_grid[-1], limit=500,
+                points=fermi_energy_eV)
+        elif integration_method == "analytical":
+            number_of_electrons = analytical_integration(dos_data, "F0", "F1",
+                                                         fermi_energy_eV,
+                                                         energy_grid,
+                                                         temperature_K)
+            band_energy_minus_uN = analytical_integration(dos_data, "F1", "F2",
+                                                          fermi_energy_eV,
+                                                          energy_grid,
+                                                          temperature_K)
+            band_energy = band_energy_minus_uN+fermi_energy_eV *\
+                number_of_electrons
+        else:
+            raise Exception("Unknown integration method.")
+
+        return band_energy
+
+    @staticmethod
+    def __entropy_contribution_from_dos(dos_data, energy_grid, fermi_energy_eV,
+                                        temperature_K, integration_method):
+        r"""
+        Calculate the entropy contribution to the total energy from DOS data.
+
+        More specifically, this gives -\beta^-1*S_S
+        """
+        # Calculate the entropy contribution to the energy.
+        if integration_method == "trapz":
+            multiplicator = entropy_multiplicator(energy_grid,
+                                                  fermi_energy_eV,
+                                                  temperature_K)
+            entropy_contribution = integrate.trapz(dos_data * multiplicator,
+                                                   energy_grid, axis=-1)
+            entropy_contribution /= get_beta(temperature_K)
+        elif integration_method == "simps":
+            multiplicator = entropy_multiplicator(energy_grid, fermi_energy_eV,
+                                                  temperature_K)
+            entropy_contribution = integrate.simps(dos_data * multiplicator,
+                                                   energy_grid, axis=-1)
+            entropy_contribution /= get_beta(temperature_K)
+        elif integration_method == "quad":
+            dos_pointer = interpolate.interp1d(energy_grid, dos_data)
+            entropy_contribution, abserr = integrate.quad(
+                lambda e: dos_pointer(e) *
+                entropy_multiplicator(e, fermi_energy_eV, temperature_K),
+                energy_grid[0], energy_grid[-1], limit=500,
+                points=fermi_energy_eV)
+            entropy_contribution /= get_beta(temperature_K)
+        elif integration_method == "analytical":
+            entropy_contribution = analytical_integration(dos_data, "S0", "S1",
+                                                          fermi_energy_eV,
+                                                          energy_grid,
+                                                          temperature_K)
+        else:
+            raise Exception("Unknown integration method.")
+
+        return entropy_contribution
