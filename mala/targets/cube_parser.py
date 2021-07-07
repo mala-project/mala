c6caacd8631745e63800e4efc9e6d4586d426216
Lenz Fiedler <l.fiedler@hzdr.de>
2021-07-07 15:08:29 +0200

    Fixed documentation in cube_parser.py

--- mala/targets/cube_parser.py
+++ mala/targets/cube_parser.py
@@ -1,57 +1,60 @@
 """
 Cube parser, taken from cubetools (see below).
 
-#------------------------------------------------------------------------------
-# Module: cubetools
-#------------------------------------------------------------------------------
-#
-# Description:
-# Module to work with Gaussian cube format files
-# (see http://paulbourke.net/dataformats/cube/)
-#
-#------------------------------------------------------------------------------
-#
-# What does it do:
-# * Read/write cube files to/from numpy arrays (dtype=float*)
-# * Read/write pairse of cube files to/from numpy arrays (dtype=complex*)
-# * Provides a CubeFile object, to be used when cubefiles with 
-#   constant and static data is required. It simulates the readline method
-#   of a file object with a cube file opened, without creating a file
-#
-#------------------------------------------------------------------------------
-#
-# Dependency: numpy
-#
-#------------------------------------------------------------------------------
-#
-# Author: P. R. Vaidyanathan (aditya95sriram <at> gmail <dot> com)
-# Date: 25th June 2017
-#
-#------------------------------------------------------------------------------
-#
-# MIT License
-# 
-# Copyright (c) 2019 P. R. Vaidyanathan
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-#------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+Module: cubetools
+
+------------------------------------------------------------------------------
+
+Description:
+Module to work with Gaussian cube format files
+(see http://paulbourke.net/dataformats/cube/)
+
+------------------------------------------------------------------------------
+
+What does it do:
+
+- Read/write cube files to/from numpy arrays (dtype=float*)
+- Read/write pairse of cube files to/from numpy arrays (dtype=complex*)
+- Provides a CubeFile object, to be used when cubefiles with constant and
+  static data is required. It simulates the readline method
+  of a file object with a cube file opened, without creating a file
+
+------------------------------------------------------------------------------
+
+Dependency: numpy
+
+------------------------------------------------------------------------------
+
+Author: P. R. Vaidyanathan (aditya95sriram <at> gmail <dot> com)
+Date: 25th June 2017
+
+------------------------------------------------------------------------------
+
+MIT License
+
+Copyright (c) 2019 P. R. Vaidyanathan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+------------------------------------------------------------------------------
 """
 import numpy as np
 
