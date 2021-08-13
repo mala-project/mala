1237968cfbffb72883d8c0dbad522772b6e33f7e
Daniel Kotik <d.kotik@hzdr.de>
2021-08-13 13:48:57 +0200

    Update `setup.py`:
    - fix license from MIT to BSD 3-Clause (we read in the license file)
    - add "et al." to indicate other authors
    - read in `README.md` for long description

--- setup.py
+++ setup.py
@@ -8,14 +8,21 @@ version = {}
 with open("mala/version.py") as fp:
     exec(fp.read(), version)
 
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
 setup(
     name="mala",
     version=version["__version__"],
     description="Framework for Electronic Structure Learning",
+    long_description=readme,
     url="https://gitlab.com/hzdr/mala/mala",
-    author="Lenz Fiedler",
+    author="Lenz Fiedler et al.",
     author_email="l.fiedler@hzdr.de",
-    license="MIT",
+    license=license,
     packages=["mala"],
     zip_safe=False,
     install_requires=open('requirements.txt').read().splitlines(),
