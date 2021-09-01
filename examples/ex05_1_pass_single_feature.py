65bffc27fa12280848dee06ce2bc27231502b850
Steve Schmerler <git@elcorto.com>
2021-09-01 21:42:46 +0200

    ex05_1: more comments, add sanity check, revert to os.path

--- examples/ex05_1_pass_single_feature.py
+++ examples/ex05_1_pass_single_feature.py
@@ -1,10 +1,14 @@
-"""Show how to run a sinlge feature vector out of a snapshot through a network.
+"""Show how to run a single feature vector out of a snapshot through a network.
+We load a pre-trained net and pass a single input feature vector through,
+including input and output scaling.
 
 First run ex05_training_with_postprocessing.py to create the
 ex05_* files used below.
 """
 
-import pathlib as pl
+import os
+
+pj = os.path.join
 
 import mala
 import numpy as np
@@ -13,7 +17,7 @@ import torch
 from data_repo_path import data_repo_path
 
 
-data_path = pl.Path(data_repo_path) / "Al36"
+data_path = pj(data_repo_path, "Al36")
 params_path = "ex05_params.pkl"
 network_path = "ex05_network.pth"
 input_scaler_path = "ex05_iscaler.pkl"
@@ -25,7 +29,7 @@ def load_whole_snapshot():
     single feature vector afterwards.
     """
 
-    # First load Parameters and network.
+    # First load Parameters.
     parameters = mala.Parameters.load_from_file(params_path, no_snapshots=True)
 
     # Load a network from a file.
@@ -41,6 +45,10 @@ def load_whole_snapshot():
 
     # Add snapshots that are to be tested and make sure that the
     # data_splitting_snapshots list is correct.
+    #
+    # Units: output_units="1/Ry" means that the output (target) data (LDOS) has
+    # unit 1/Ry. Rescaled network output, after oscaler.inverse_transform() is
+    # applied (see below) will be in 1/eV.
     parameters.data.data_splitting_snapshots = ["te"]
     inference_data_handler.add_snapshot(
         "Al_debug_2k_nr2.in.npy",
@@ -51,10 +59,11 @@ def load_whole_snapshot():
     )
     inference_data_handler.prepare_data(reparametrize_scaler=False)
 
-    # Extract single feature vector from snapshot. The x,y,z part (first there
-    # entries) are removed in DataHandler (see load_memmap_snapshot() for
-    # details) and parameters.descriptors_contain_xyz, which is True by
-    # default. So the length is 91.
+    # Extract single feature vector from snapshot. The x,y,z part (first 3
+    # entries) are removed in DataHandler, so the feature vector length is 91
+    # instead of 94. See also load_memmap_snapshot() below for details. The
+    # removal in DataHandler is controlled by
+    # parameters.descriptors_contain_xyz, which is True by default.
     x_scaled = inference_data_handler.test_data_inputs[0]
     with torch.no_grad():
         y_pred = network(x_scaled)
@@ -69,8 +78,9 @@ def load_memmap_snapshot():
     """Load numpy memmap of snapshot and extract single feature. Avoid loading
     whole snapshot into memory.
     """
-    # First load Parameters and network.
+    # First load Parameters.
     parameters = mala.Parameters.load_from_file(params_path, no_snapshots=True)
+
     # Load a network from a file.
     network = mala.Network.load_from_file(parameters, network_path)
 
@@ -79,7 +89,9 @@ def load_memmap_snapshot():
     oscaler = mala.DataScaler.load_from_file(output_scaler_path)
 
     # Load snapshot with input features, use memmap.
-    inputs_array = np.load(data_path / "Al_debug_2k_nr2.in.npy", mmap_mode="r")
+    inputs_array = np.load(
+        pj(data_path, "Al_debug_2k_nr2.in.npy"), mmap_mode="r"
+    )
 
     # Our inputs usually contain x,y,z information, so one single input vector
     # reads [x,y,z,f_0, f_1, ..., f_N] where [f_0, f_1, ..., f_N] is the actual
@@ -87,8 +99,6 @@ def load_memmap_snapshot():
     # snapshot.
     x = torch.from_numpy(inputs_array[0, 0, 0, 3:].astype(np.float32))
 
-    # Convert to tensor and scale.
-    #
     # For some reason, DataScaler changes the shape (N,) -> (1,N), revert that
     x_scaled = iscaler.transform(x)[0, :]
     with torch.no_grad():
@@ -101,6 +111,18 @@ def load_memmap_snapshot():
 
 
 if __name__ == "__main__":
+    # fmt: off
+    assert os.path.exists(data_path), f"{data_path} missing, link data repo first"
+    # fmt: on
+
+    for pth in [
+        params_path,
+        network_path,
+        input_scaler_path,
+        output_scaler_path,
+    ]:
+        assert os.path.exists(pth), f"{pth} missing, run ex05 first"
+
     a = load_whole_snapshot()
     b = load_memmap_snapshot()
 
