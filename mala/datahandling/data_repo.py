daaf544e16e8934aca620191f891996c3669c3b8
Steve Schmerler <git@elcorto.com>
2021-11-11 16:35:28 +0100

    data_repo: add module doc string

--- mala/datahandling/data_repo.py
+++ mala/datahandling/data_repo.py
@@ -1,8 +1,15 @@
+"""
+Compatibility layer for tests and examples.
+
+Provide access to the MALA_DATA_REPO env var via the module level
+`data_repo_path` attribute.
+
+MALA_DATA_REPO should point to /path/to/test-data which is a checkout of
+https://github.com/mala-project/test-data .
+"""
+
 import os
 
-# This env var should point to /path/to/test-data which is a checkout of
-# https://github.com/mala-project/test-data . Only used in unit tests and
-# examples.
 name = "MALA_DATA_REPO"
 assert name in os.environ, f"Environment variable {name} not set."
 data_repo_path = os.environ[name]
