3a1b481ab833c448c65b779602028e2f5591ecab
Lenz Fiedler <l.fiedler@hzdr.de>
2022-09-28 15:58:35 +0200

    Last touches

--- external_modules/total_energy_module/prepare_qe.sh
+++ external_modules/total_energy_module/prepare_qe.sh
@@ -10,6 +10,6 @@ err(){
 [ $# -eq 1 ] || err "Please provide exactly one argument (the path to the QE directory)" && root_dir=$1
 
 cd $root_dir
-if [ "$(grep -q "\$foxflags -fPIC" install/configure)" == "" ]; then
+if ! $(grep -q "\$foxflags -fPIC" install/configure); then
   sed -i 's/# Checking preprocessor.../foxflags="$foxflags -fPIC"\n# Checking preprocessor.../g' install/configure
 fi
