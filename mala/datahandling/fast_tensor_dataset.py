1e4ac018f4ff72f44d9137ef8d45300f2fbd1eca
Lenz Fiedler <l.fiedler@hzdr.de>
2023-01-18 09:45:14 +0100

    Added doc on performance changes as well as test

--- mala/datahandling/fast_tensor_dataset.py
+++ mala/datahandling/fast_tensor_dataset.py
@@ -20,12 +20,29 @@ class FastTensorDataset(torch.utils.data.Dataset):
         self.len = total_samples // self.batch_size
 
     def __getitem__(self, idx):
+        """
+        Get an item from the data set.
+
+        This returns an entire batch
+
+        Parameters
+        ----------
+        idx : int
+            Batch Index.
+
+        Returns
+        -------
+        batch : tuple
+            The data tuple for this batch.
+        """
         batch = self.indices[idx*self.batch_size:(idx+1)*self.batch_size]
         rv = tuple(t[batch, ...] for t in self.tensors)
         return rv
 
     def __len__(self):
+        """Get the length of the data set."""
         return self.len
 
     def shuffle(self):
+        """Shuffle the data set."""
         np.random.shuffle(self.indices)
