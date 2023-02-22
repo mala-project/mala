4cb309028eba47c3ff686e0e35e27bf77ce8a807
Lenz Fiedler <l.fiedler@hzdr.de>
2023-02-22 10:21:58 +0100

    Updated example

--- examples/ex19_minterpy_descriptors.py
+++ examples/ex19_minterpy_descriptors.py
@@ -9,7 +9,9 @@ outfile = os.path.join(data_path, "Be_snapshot0.out")
 params = mala.Parameters()
 params.descriptors.atomic_density_cutoff = 4.0
 params.descriptors.descriptors_contain_xyz = True
-params.descriptors.minterpy_cutoff_cube_size = 1.0
+params.descriptors.minterpy_cutoff_cube_size = 4.0
+params.descriptors.minterpy_polynomial_degree = 4
+params.descriptors.minterpy_lp_norm = 2
 
 descriptor_calculator = mala.MinterpyDescriptors(params)
 data = descriptor_calculator.calculate_from_qe_out(outfile)[0]
