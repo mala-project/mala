f26ef6841db0aee1a56866f0e1f1028d40ed1e8e
Kyle Miller <kdmille@sandia.gov>
2023-03-03 14:12:39 -0700

    DEBUG/TEMPORARY: commenting out total energy import to prevent bug, changed default save/load to not zipped

--- mala/network/runner.py
+++ mala/network/runner.py
@@ -42,7 +42,7 @@ class Runner:
         self.data = data
         self.__prepare_to_run()
 
-    def save_run(self, run_name, save_path="./", zip_run=True,
+    def save_run(self, run_name, save_path="./", zip_run=False,
                  save_runner=False):
         """
         Save the current run.
@@ -88,7 +88,7 @@ class Runner:
                     os.remove(os.path.join(save_path, file))
 
     @classmethod
-    def load_run(cls, run_name, path="./", zip_run=True,
+    def load_run(cls, run_name, path="./", zip_run=False,
                  params_format="json", load_runner=True,
                  prepare_data=False):
         """
@@ -180,7 +180,7 @@ class Runner:
             return loaded_params, loaded_network, new_datahandler
 
     @classmethod
-    def run_exists(cls, run_name, params_format="json", zip_run=True):
+    def run_exists(cls, run_name, params_format="json", zip_run=False):
         """
         Check if a run exists.
 
@@ -309,6 +309,8 @@ class Runner:
         new_batch_size = batchsize
         if datasize % new_batch_size != 0:
             while datasize % new_batch_size != 0:
+                if new_batch_size > datasize:
+                    new_batch_size = new_batch_size//2
                 new_batch_size += 1
         return new_batch_size
 
