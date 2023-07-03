61ab8fa769e816a683177e7fb98572a6089b4c32
Lenz Fiedler <l.fiedler@hzdr.de>
2023-07-03 16:39:07 +0200

    Adapted build for QE7.2

--- external_modules/total_energy_module/build_total_energy_module.sh
+++ external_modules/total_energy_module/build_total_energy_module.sh
@@ -35,9 +35,6 @@ modobjs="$(find $root_dir/Modules/ -name "*.o")"
 [ -n "$utilobjs" ] || err "utilobjs empty"
 [ -n "$modobjs" ] || err "modobjs empty"
 
-# Libraries from the external FoX project
-foxlibs="-L$root_dir/FoX/lib -lFoX_dom -lFoX_utils -lFoX_wcml -lFoX_wkml -lFoX_wxml -lFoX_common -lFoX_sax -lFoX_common -lFoX_fsys"
-
 project_lib_folders=" -L$root_dir/Modules -L$root_dir/KS_Solvers -L$root_dir/FFTXlib/src -L$root_dir/LAXlib -L$root_dir/UtilXlib -L$root_dir/dft-d3 -L$root_dir/upflib -L$root_dir/XClib -L$root_dir/external/devxlib/src -L$root_dir/external/mbd/src"
 project_libs="-lqemod -lks_solvers -lqefft -lqela -lutil -ldftd3qe -lupf -ldevXlib -lmbd"
 project_inc_folders="-I$root_dir/Modules -I$root_dir/FFTXlib/src -I$root_dir/LAXlib -I$root_dir/KS_Solvers -I$root_dir/UtilXlib -I$root_dir/upflib -I$root_dir/XClib -I$root_dir/external/devxlib/src -I$root_dir/external/mbd/src"
@@ -67,7 +64,6 @@ ${F2PY:=f2py} \
     $modobjs \
     $linalg \
     $fftw \
-    $foxlibs \
     $root_dir/XClib/xc_lib.a
 
 rm $src
