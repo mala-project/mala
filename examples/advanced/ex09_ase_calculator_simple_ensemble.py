abbd1c557d277f4f0debc6ebb23dc15fe8cc730b
Steve Schmerler <git@elcorto.com>
2024-03-18 15:10:54 +0100

    advanced/ex09: format code

--- examples/advanced/ex09_ase_calculator_simple_ensemble.py
+++ examples/advanced/ex09_ase_calculator_simple_ensemble.py
@@ -11,9 +11,11 @@ from mala import printout
 from ase.io import read
 
 from mala.datahandling.data_repo import data_repo_path
+
 data_path = os.path.join(data_repo_path, "Be2")
 
-def message(txt, bar="="*79):
+
+def message(txt, bar="=" * 79):
     print(f"\n{bar}\n{txt}\n{bar}\n")
 
 
@@ -32,25 +34,39 @@ def train_network_ensemble(n_models=5):
         parameters.running.trainingtype = "Adam"
 
         data_handler = mala.DataHandler(parameters)
-        data_handler.add_snapshot("Be_snapshot0.in.npy", data_path,
-                                  "Be_snapshot0.out.npy", data_path, "tr")
-        data_handler.add_snapshot("Be_snapshot1.in.npy", data_path,
-                                  "Be_snapshot1.out.npy", data_path, "va")
+        data_handler.add_snapshot(
+            "Be_snapshot0.in.npy",
+            data_path,
+            "Be_snapshot0.out.npy",
+            data_path,
+            "tr",
+        )
+        data_handler.add_snapshot(
+            "Be_snapshot1.in.npy",
+            data_path,
+            "Be_snapshot1.out.npy",
+            data_path,
+            "va",
+        )
         data_handler.prepare_data()
         printout("Read data: DONE.")
 
         parameters.network.layer_sizes = [
-              data_handler.input_dimension,
-              100,
-              data_handler.output_dimension]
+            data_handler.input_dimension,
+            100,
+            data_handler.output_dimension,
+        ]
 
         network = mala.Network(parameters)
         trainer = mala.Trainer(parameters, network, data_handler)
         trainer.train_network()
-        additional_calculation_data = os.path.join(data_path,
-                                                   "Be_snapshot0.out")
-        trainer.save_run(f"be_model_test{idx}",
-                         additional_calculation_data=additional_calculation_data)
+        additional_calculation_data = os.path.join(
+            data_path, "Be_snapshot0.out"
+        )
+        trainer.save_run(
+            f"be_model_test{idx}",
+            additional_calculation_data=additional_calculation_data,
+        )
 
 
 # Load a model.
@@ -79,6 +95,8 @@ calculator.mala_parameters.targets.pseudopotential_path = data_path
 message("Calculate total energy for ensemble")
 atoms.set_calculator(calculator)
 atoms.get_potential_energy()
-print(calculator.results["energy"],
-      calculator.results["energy_std"],
-      calculator.results["energy_samples"])
+print(
+    calculator.results["energy"],
+    calculator.results["energy_std"],
+    calculator.results["energy_samples"],
+)
