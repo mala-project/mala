ac20a96189cac2766622c3352a0b5af1dec2205b
Steve Schmerler <git@elcorto.com>
2024-04-18 23:17:33 +0200

    Format setup.py and docs/source/conf.py

--- setup.py
+++ setup.py
@@ -15,27 +15,30 @@ with open("LICENSE") as f:
     license = f.read()
 
 extras = {
-    'dev': ['bump2version'],
-    'opt': ['oapackage'],
-    'test': ['pytest'],
-    'doc': open('docs/requirements.txt').read().splitlines(),
-    'experimental': ['asap3', 'dftpy', 'minterpy']
+    "dev": ["bump2version"],
+    "opt": ["oapackage"],
+    "test": ["pytest"],
+    "doc": open("docs/requirements.txt").read().splitlines(),
+    "experimental": ["asap3", "dftpy", "minterpy"],
 }
 
 setup(
     name="materials-learning-algorithms",
     version=version["__version__"],
-    description=("Materials Learning Algorithms. "
-                 "A framework for machine learning materials properties from "
-                 "first-principles data."),
+    description=(
+        "Materials Learning Algorithms. "
+        "A framework for machine learning materials properties from "
+        "first-principles data."
+    ),
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://github.com/mala-project/mala",
     author="MALA developers",
     license=license,
-    packages=find_packages(exclude=("test", "docs", "examples", "install",
-                                    "ml-dft-sandia")),
+    packages=find_packages(
+        exclude=("test", "docs", "examples", "install", "ml-dft-sandia")
+    ),
     zip_safe=False,
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires=open("requirements.txt").read().splitlines(),
     extras_require=extras,
 )
