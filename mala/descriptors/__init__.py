01d909f857d953943c0d5a00cae911693b1f32f1
Callow <timjcallow@gmail.com>
2024-05-31 16:14:45 +0200

    Add ACE to __init__

--- mala/descriptors/__init__.py
+++ mala/descriptors/__init__.py
@@ -4,3 +4,4 @@ from .bispectrum import Bispectrum
 from .atomic_density import AtomicDensity
 from .descriptor import Descriptor
 from .minterpy_descriptors import MinterpyDescriptors
+from .ace import ACE
