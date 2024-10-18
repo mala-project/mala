28ed972d6c901b09df4d12e8bb6262e552a435dd
Lenz Fiedler <l.fiedler@hzdr.de>
2024-10-18 12:01:07 +0200

    Fixed warning

--- mala/interfaces/ase_calculator.py
+++ mala/interfaces/ase_calculator.py
@@ -3,7 +3,7 @@
 from ase.calculators.calculator import Calculator, all_changes
 
 from mala import Parameters, Network, DataHandler, Predictor, LDOS
-from mala.common.parallelizer import barrier
+from mala.common.parallelizer import barrier, parallel_warn
 
 
 class MALA(Calculator):
@@ -78,6 +78,30 @@ class MALA(Calculator):
 
     @classmethod
     def load_model(cls, run_name, path="./"):
+        """
+        Load a model to use for the calculator.
+
+        Only supports zipped models with .json parameters. No legacy
+        models supported.
+
+        Parameters
+        ----------
+        run_name : str
+            Name under which the model is saved.
+
+        path : str
+            Path where the model is saved.
+        """
+        parallel_warn(
+            "MALA.load_model() will be deprecated in MALA v1.4.0."
+            " Please use MALA.load_run() instead.",
+            0,
+            category=FutureWarning,
+        )
+        return MALA.load_run(run_name, path=path)
+
+    @classmethod
+    def load_run(cls, run_name, path="./"):
         """
         Load a model to use for the calculator.
 
