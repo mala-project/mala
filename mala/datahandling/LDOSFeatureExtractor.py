64c806ea8b7c93e4e9da81f1c909d819fb39499e
Lenz Fiedler <l.fiedler@hzdr.de>
2025-02-14 09:39:48 +0100

    Serial version finished

--- mala/datahandling/LDOSFeatureExtractor.py
+++ mala/datahandling/LDOSFeatureExtractor.py
@@ -1,6 +1,6 @@
 import os.path
 
-from mala import printout
+from mala import printout, get_rank, get_size
 from mala.targets.target import Target
 import numpy as np
 from scipy.fft import rfft, irfft
@@ -11,54 +11,99 @@ import time
 
 class LDOSFeatureExtractor:
 
-    def __init__(self, parameters, target_calculator=None):
+    def __init__(
+        self,
+        parameters,
+        target_calculator=None,
+        rescaling_factor=1000,
+        number_of_gaussians=8,
+        maximum_number_of_retries=7,
+    ):
         self.parameters = parameters
         self.target_calculator = target_calculator
         if self.target_calculator is None:
             self.target_calculator = Target(parameters)
+        self.rescaling_factor = rescaling_factor
+        self.number_of_gaussians = number_of_gaussians
+        self.maximum_number_of_retries = maximum_number_of_retries
 
-    def extract_features(self, ldos_file, cutoff=30, n_gaussians=8):
+    def extract_features(self, ldos_file, cutoff=30, dry_run=False):
         # Initialize everything needed for the fit.
         initial_guess_left = self.__initial_guess_linear()
         initial_guess_right = self.__initial_guess_linear()
-        initial_guess_gaussian = self.__initial_guess_gaussians(n_gaussians)
+        initial_guess_gaussian = self.__initial_guess_gaussians()
 
         # Extract the basename of the file
         path = os.path.dirname(ldos_file)
-        filename = os.path.basename(ldos_file)
-        extracted_filename = os.path.join(path, filename, "_extracted.npy")
+        filename = os.path.basename(ldos_file).split(".")[0]
+        extracted_filename = os.path.join(path, filename + "_extracted.npy")
+        filtered_filename = os.path.join(path, filename + "_filtered.npy")
 
         # Load and filter the LDOS.
-        ldos = self.target_calculator.read_from_numpy_file(ldos_file)[
-            :40, :40, :80, :
-        ]
+        if self.parameters.use_mpi:
+
+            # This is only a quick and dirty implementation. I am splitting
+            # The LDOS data across the ranks along the z-axis, for the
+            # sole reason that makes sense for graphene cells.
+            # This is not a general solution, and should be replaced by one in
+            # the future.
+            ldos_pointer = np.load(ldos_file, mmap_mode="r")
+            ldos_shape = np.shape(ldos_pointer)
+            full_gridsize = ldos_shape[0] * ldos_shape[1] * ldos_shape[2]
+            z_size = ldos_shape[2] // get_size()
+            z_start = get_rank() * z_size
+            z_end = (get_rank() + 1) * z_size
+            if get_rank() == get_size() - 1:
+                z_end = ldos_shape[2]
+            ldos = ldos_pointer[:, :, z_start:z_end, :]
+            gridsize = ldos_shape[0] * ldos_shape[1] * (z_end - z_start)
+            ldos_shape = np.shape(ldos)
+        else:
+            ldos = self.target_calculator.read_from_numpy_file(ldos_file)
+            ldos_shape = np.shape(ldos)
+            gridsize = ldos_shape[0] * ldos_shape[1] * ldos_shape[2]
+
+        ldos = ldos.reshape(
+            [
+                gridsize,
+                ldos_shape[3],
+            ]
+        )
+        if dry_run:
+            ldos = ldos[
+                np.random.randint(
+                    0, ldos_shape[0] * ldos_shape[1] * ldos_shape[2], size=100
+                ),
+                :,
+            ]
+            gridsize = 100
         # TODO: The FFT changes the dimensions from 301 to 300 - this is OK
         # for the moment since we are interested in qualitative results,
         # but should eventually be fixed!
         ldos = self.__lowpass_filter(ldos, cutoff=cutoff)
+        if dry_run:
+            np.save(filtered_filename, ldos)
+        ldos *= self.rescaling_factor
         ldos_shape = np.shape(ldos)
-        ldos = ldos.reshape(
-            [
-                ldos_shape[0] * ldos_shape[1] * ldos_shape[2],
-                ldos_shape[3],
-            ]
-        )
-        gridsize = ldos_shape[0] * ldos_shape[1] * ldos_shape[2]
         xdata = np.arange(ldos_shape[-1])
 
         # 5 = 2 for left linear function, 2 for right linear function, 1 for
         # position of minimum.
-        features = np.zeros((gridsize, 5 + 3 * n_gaussians), dtype=ldos.dtype)
+        features = np.zeros(
+            (gridsize, 5 + 3 * self.number_of_gaussians), dtype=ldos.dtype
+        )
 
         # Time for-loop.
         start_time = time.perf_counter()
 
         # For each point within the LDOS, perform a fit to extract the
         # features.
+        self.maximum_number_of_retries = 7
+
         for point in range(0, gridsize):
             ydata = ldos[point, :]
             trial_counter = 0
-            while trial_counter < 5:
+            while trial_counter < self.maximum_number_of_retries:
                 try:
                     features[point, :] = self.composite_fit(
                         xdata,
@@ -66,106 +111,113 @@ class LDOSFeatureExtractor:
                         initial_guess_left,
                         initial_guess_right,
                         initial_guess_gaussian,
-                        n_gaussians,
                     )
                     break
                 except RuntimeError:
                     trial_counter += 1
                     printout("Fit failed at point ", point, ", retrying.")
-            if trial_counter == 3:
-                raise Exception("Fit failed at point ", point, "three times.")
 
-            initial_guess_left = features[point, 1:3]
-            initial_guess_right = features[point, 3:5]
-            initial_guess_gaussian = features[point, 5:]
+                    # Retrying with different initial guess.
+                    initial_guess_left = self.__initial_guess_linear()
+                    initial_guess_right = self.__initial_guess_linear()
+                    initial_guess_gaussian = self.__initial_guess_gaussians()
+
+            if trial_counter == self.maximum_number_of_retries:
+                printout(
+                    "Fit failed at point ",
+                    self.maximum_number_of_retries,
+                    " times. Zeroing out features.",
+                )
+                features[point, :] = self.__zero_out_features(ydata)
+
+            if not dry_run:
+                initial_guess_left = features[point, 1:3]
+                initial_guess_right = features[point, 3:5]
+                initial_guess_gaussian = features[point, 5:]
             if point % 100 == 0:
                 print("Point: ", point, "/", gridsize)
                 print(
                     "Time per point: ",
                     (time.perf_counter() - start_time) / (point + 1),
                 )
+        np.save(extracted_filename, features)
 
-    @staticmethod
+    def reconstruct_ldos(
+        self, ldos_features, original_dimension=300, n_gaussians=8
+    ):
+        xdata = np.arange(original_dimension)
+        splitting_x = int(ldos_features[0])
+        x_left = slice(0, splitting_x, 1)
+        x_right = slice(splitting_x, original_dimension, 1)
+
+        predicted_left = self.linear(xdata[x_left], *ldos_features[1:3])
+        predicted_right = self.linear(xdata[x_right], *ldos_features[3:5])
+        predicted_twolinear = np.concatenate((predicted_left, predicted_right))
+        predicted_gaussian = self.gaussians(xdata, *ldos_features[5:])
+
+        return (
+            predicted_twolinear + predicted_gaussian
+        )  # / self.rescaling_factor
+
+    def __zero_out_features(self, ydata):
+        splitting_x = int(np.shape(ydata)[0] / 2)
+        popt_left = [0, 0]
+        popt_right = [0, 0]
+        popt_2 = np.concatenate(
+            (
+                np.zeros(self.number_of_gaussians),
+                np.arange(
+                    np.shape(ydata)[0] / (self.number_of_gaussians + 1),
+                    np.shape(ydata)[0],
+                    step=np.shape(ydata)[0] / (self.number_of_gaussians + 1),
+                ),
+                np.zeros(self.number_of_gaussians) + np.sqrt(0.5),
+            )
+        )
+
+        return np.concatenate(([splitting_x], popt_left, popt_right, popt_2))
+
     def composite_fit(
+        self,
         xdata,
         ydata,
         initial_guess_left,
         initial_guess_right,
         initial_guess_gaussian,
-        n_gaussians,
     ):
-        def linear(x, *params):
-            params = np.array(params)
-            poly_params = params
-            x = x.reshape(-1, 1)
-            poly = np.sum(poly_params * x ** np.arange(2), axis=-1)
-            return poly
-
-        def gaussians(x, *params):
-            params = np.array(params)
-            gauss_params = params
-            weights = gauss_params[:n_gaussians]
-            means = gauss_params[n_gaussians : 2 * n_gaussians]
-            sigmas = gauss_params[2 * n_gaussians :]
-            x = x.reshape(-1, 1)
-            gauss = np.sum(
-                weights * np.exp(-((x - means) ** 2) / (2 * sigmas**2)),
-                axis=-1,
-            )
-            return gauss
-
-        def gaussians_error(x, *params):
-            return gaussians(x, *params) - ydata
-
         if np.sum(ydata) == 0:
-            splitting_x = int(np.shape(ydata)[0] / 2)
-            popt_left = [0, 0]
-            popt_right = [0, 0]
-            popt_2 = np.concatenate(
-                (
-                    np.zeros(n_gaussians),
-                    np.arange(
-                        np.shape(ydata)[0] / (n_gaussians + 1),
-                        np.shape(ydata)[0],
-                        step=np.shape(ydata)[0] / (n_gaussians + 1),
-                    ),
-                    np.zeros(n_gaussians) + np.sqrt(0.5),
-                )
-            )
-
+            return self.__zero_out_features(ydata)
         else:
             splitting_x = np.argmin(ydata)
             x_left = slice(0, splitting_x, 1)
             x_right = slice(splitting_x, np.shape(ydata)[0], 1)
 
             popt_left, pcov_left = curve_fit(
-                linear,
+                self.linear,
                 xdata[x_left],
                 ydata[x_left],
                 p0=initial_guess_left,
-                # bounds=bounds,
                 maxfev=10000,
                 method="trf",
             )
             popt_right, pcov_right = curve_fit(
-                linear,
+                self.linear,
                 xdata[x_right],
                 ydata[x_right],
                 p0=initial_guess_right,
-                # bounds=bounds,
                 maxfev=10000,
                 method="trf",
             )
 
-            predicted_left = linear(xdata[x_left], *popt_left)
-            predicted_right = linear(xdata[x_right], *popt_right)
+            predicted_left = self.linear(xdata[x_left], *popt_left)
+            predicted_right = self.linear(xdata[x_right], *popt_right)
             predicted_twolinear = np.concatenate(
                 (predicted_left, predicted_right)
             )
             reduced = ydata - predicted_twolinear
 
             popt_2, pcov_2 = curve_fit(
-                gaussians_error,
+                self.gaussians,
                 xdata,
                 reduced,
                 p0=initial_guess_gaussian,
@@ -173,16 +225,48 @@ class LDOSFeatureExtractor:
                 method="trf",
             )
 
-            ordering = np.argsort(popt_2[n_gaussians : n_gaussians * 2])
-            popt_2[0:n_gaussians] = popt_2[0:n_gaussians][ordering]
-            popt_2[n_gaussians : n_gaussians * 2] = popt_2[
-                n_gaussians : n_gaussians * 2
+            ordering = np.argsort(
+                popt_2[self.number_of_gaussians : self.number_of_gaussians * 2]
+            )
+            popt_2[0 : self.number_of_gaussians] = popt_2[
+                0 : self.number_of_gaussians
             ][ordering]
-            popt_2[n_gaussians * 2 :] = popt_2[n_gaussians * 2 :][ordering]
+            popt_2[self.number_of_gaussians : self.number_of_gaussians * 2] = (
+                popt_2[
+                    self.number_of_gaussians : self.number_of_gaussians * 2
+                ][ordering]
+            )
+            popt_2[self.number_of_gaussians * 2 :] = popt_2[
+                self.number_of_gaussians * 2 :
+            ][ordering]
 
             # Implement error metric
 
-        return np.concatenate(([splitting_x], popt_left, popt_right, popt_2))
+            return np.concatenate(
+                ([splitting_x], popt_left, popt_right, popt_2)
+            )
+
+    def linear(self, x, *params):
+        params = np.array(params)
+        poly_params = params
+        x = x.reshape(-1, 1)
+        poly = np.sum(poly_params * x ** np.arange(2), axis=-1)
+        return poly
+
+    def gaussians(self, x, *params):
+        params = np.array(params)
+        gauss_params = params
+        weights = gauss_params[: self.number_of_gaussians]
+        means = gauss_params[
+            self.number_of_gaussians : 2 * self.number_of_gaussians
+        ]
+        sigmas = gauss_params[2 * self.number_of_gaussians :]
+        x = x.reshape(-1, 1)
+        gauss = np.sum(
+            weights * np.exp(-((x - means) ** 2) / (2 * sigmas**2)),
+            axis=-1,
+        )
+        return gauss
 
     @staticmethod
     def __lowpass_filter(ldos, cutoff):
@@ -200,21 +284,20 @@ class LDOSFeatureExtractor:
         # scale to bounds
         return lbound_poly + (ubound_poly - lbound_poly) * p0
 
-    @staticmethod
-    def __initial_guess_gaussians(n_gaussians):
-        lbound_weights = np.ones(n_gaussians) * -100
-        ubound_weights = np.ones(n_gaussians) * 100
+    def __initial_guess_gaussians(self):
+        lbound_weights = np.ones(self.number_of_gaussians) * -100
+        ubound_weights = np.ones(self.number_of_gaussians) * 100
 
-        lbound_means = np.zeros(n_gaussians)
-        ubound_means = np.ones(n_gaussians) * 300
+        lbound_means = np.zeros(self.number_of_gaussians)
+        ubound_means = np.ones(self.number_of_gaussians) * 300
 
-        lbound_sigmas = np.ones(n_gaussians) * 0.01
-        ubound_sigmas = np.ones(n_gaussians) * 100
+        lbound_sigmas = np.ones(self.number_of_gaussians) * 0.01
+        ubound_sigmas = np.ones(self.number_of_gaussians) * 100
 
         lbounds = np.concatenate([lbound_weights, lbound_means, lbound_sigmas])
         ubounds = np.concatenate([ubound_weights, ubound_means, ubound_sigmas])
 
-        parameter_space_dim_poly = 3 * n_gaussians
+        parameter_space_dim_poly = 3 * self.number_of_gaussians
         p0 = np.random.uniform(0, 1, parameter_space_dim_poly)
         # scale to bounds
         return lbounds + (ubounds - lbounds) * p0
