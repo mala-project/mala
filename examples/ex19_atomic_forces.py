Hopefully fixed the scaling issue

examples/ex19_atomic_forces.py
@@ -166,24 +166,32 @@ def hartree_contribution():
         print(mala_forces[point, :] / np.array(numerical_forces))
 
 
-def backpropagation():
+def backpropagation_pieces():
     """
-    Test whether backpropagation works. To this end, the entire forces are
-    computed, and then backpropagated through the network.
+    Check the individual parts of the backpropagation.
     """
     # Only compute a specific part of the forces.
+    ldos_calculator: mala.LDOS
+    predictor: mala.Predictor
     ldos, ldos_calculator, parameters, predictor = run_prediction(
         backprop=True
     )
-    ldos_calculator.input_data_derivative = predictor.input_data
-    ldos_calculator.output_data_torch = predictor.output_data
+    # For easier testing, can also be commented out.
+    ldos_calculator.debug_forces_flag = "band_energy"
+
+    # This next line can later be part of the prediction itself within the
+    # Predictor class.
+    ldos_calculator.setup_for_forces(predictor)
+
+    # Performs the calculation internally.
     mala_forces = ldos_calculator.atomic_forces
+
     # Should be 8748, 91
     print("FORCE TEST: Backpropagation machinery.")
     print(mala_forces.size())
 
 
-band_energy_contribution()
-entropy_contribution()
-hartree_contribution()
-backpropagation()
+# band_energy_contribution()
+# entropy_contribution()
+# hartree_contribution()
+backpropagation_pieces()

mala/network/predictor.py
@@ -46,6 +46,7 @@ class Predictor(Runner):
         self._test_data_loader = None
         self._number_of_batches_per_snapshot = 0
         self.target_calculator = data.target_calculator
+
         self.input_data = None
         self.output_data = None
 
@@ -76,8 +77,9 @@ class Predictor(Runner):
             self.data.target_calculator.atoms, gather_ldos=gather_ldos
         )
 
-    def predict_for_atoms(self, atoms, gather_ldos=False, temperature=None,
-                          save_grads=False):
+    def predict_for_atoms(
+        self, atoms, gather_ldos=False, temperature=None, save_grads=False
+    ):
         """
         Get predicted LDOS for an atomic configuration.
 
@@ -206,14 +208,15 @@ class Predictor(Runner):
             if save_grads is True:
                 self.input_data = snap_descriptors
                 self.input_data.requires_grad = True
-                return self._forward_snap_descriptors(snap_descriptors,
-                                                      save_torch_outputs=True)
+                return self._forward_snap_descriptors(
+                    snap_descriptors, save_torch_outputs=True
+                )
             else:
                 return self._forward_snap_descriptors(snap_descriptors)
 
-    def _forward_snap_descriptors(self, snap_descriptors,
-                                  local_data_size=None,
-                                  save_torch_outputs=False):
+    def _forward_snap_descriptors(
+        self, snap_descriptors, local_data_size=None, save_torch_outputs=False
+    ):
         """
         Forward a scaled tensor of descriptors through the neural network.
 
@@ -244,13 +247,13 @@ class Predictor(Runner):
 
         if local_data_size is None:
             local_data_size = self.data.grid_size
-        predicted_outputs = \
-            np.zeros((local_data_size,
-                      self.data.target_calculator.feature_size))
+        predicted_outputs = np.zeros(
+            (local_data_size, self.data.target_calculator.feature_size)
+        )
         if save_torch_outputs:
-            self.output_data = \
-                torch.zeros((local_data_size,
-                             self.data.target_calculator.feature_size))
+            self.output_data = torch.zeros(
+                (local_data_size, self.data.target_calculator.feature_size)
+            )
 
         # Only predict if there is something to predict.
         # Elsewise, we just wait at the barrier down below.
@@ -281,8 +284,7 @@ class Predictor(Runner):
                     self.parameters._configuration["device"]
                 )
                 if save_torch_outputs:
-                    self.output_data[sl] \
-                    = self.network(inputs)
+                    self.output_data[sl] = self.network(inputs)
                 predicted_outputs[sl] = (
                     self.data.output_data_scaler.inverse_transform(
                         self.network(inputs).to("cpu"), as_numpy=True

mala/targets/ldos.py
@@ -2,6 +2,7 @@
 
 from functools import cached_property
 
+import mala.datahandling.data_scaler
 from ase.units import Rydberg, Bohr, J, m
 import math
 import numpy as np
@@ -49,6 +50,10 @@ class LDOS(Target):
         self.debug_forces_flag = None
         self.input_data_derivative = None
         self.output_data_torch = None
+        self.input_data_scaler: mala.datahandling.data_scaler.DataScaler = None
+        self.output_data_scaler: mala.datahandling.data_scaler.DataScaler = (
+            None
+        )
 
     @classmethod
     def from_numpy_file(cls, params, path, units="1/(eV*A^3)"):
@@ -1606,8 +1611,21 @@ class LDOS(Target):
             #################################
 
             if self.input_data_derivative is not None:
-                self.output_data_torch.backward(torch.from_numpy(d_E_d_d))
-                d_d_d_B = self.input_data_derivative.grad
+                # OLD: This is how the code was until 14.03.2025.
+                # I think it is not yet correct - d_E_d_d is still scaled,
+                # and d_d_d_B is never unscaled.
+                # self.output_data_torch.backward(torch.from_numpy(d_E_d_d))
+                # d_d_d_B = self.input_data_derivative.grad
+
+                # I think this is how it is supposed to read:
+                self.output_data_torch.backward(
+                    self.output_data_scaler.transform(
+                        torch.from_numpy(d_E_d_d)
+                    )
+                )
+                d_d_d_B = self.input_data_scaler.inverse_transform(
+                    self.input_data_derivative.grad
+                )
                 return d_d_d_B
 
             else:
@@ -1983,3 +2001,18 @@ class LDOS(Target):
         else:
             self.local_density_of_states = ldos_data
             return ldos_data
+
+    def setup_for_forces(self, predictor):
+        """
+        Set calculator up for forces prediction by copying data from Predictor.
+
+
+        Parameters
+        ----------
+        predictor : mala.Predictor
+            Predictor class used for LDOS prediction.
+        """
+        self.input_data_derivative = predictor.input_data
+        self.output_data_torch = predictor.output_data
+        self.input_data_scaler = predictor.data.input_data_scaler
+        self.output_data_scaler = predictor.data.output_data_scaler