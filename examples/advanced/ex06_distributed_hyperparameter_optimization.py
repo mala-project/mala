f8bc86c4994a7a138ccc2ed9854f46ddc43f72e2
Lenz Fiedler <l.fiedler@hzdr.de>
2025-03-24 10:07:29 +0100

    Fixed issue with hyperparameterm optimozation

--- examples/advanced/ex06_distributed_hyperparameter_optimization.py
+++ examples/advanced/ex06_distributed_hyperparameter_optimization.py
@@ -36,15 +36,20 @@ parameters.hyperparameters.hyper_opt_method = "optuna"
 parameters.hyperparameters.study_name = "ex06"
 parameters.hyperparameters.rdb_storage = "sqlite:///ex06.db"
 
-# Hyperparameter optimization can be further refined by using ensemble training
-# at each step and by using a different metric then the validation loss
-# (e.g. the band energy). It is recommended not to use the ensemble training
-# method in Single-GPU use, as it naturally drastically decreases performance.
 parameters.targets.ldos_gridsize = 11
 parameters.targets.ldos_gridspacing_ev = 2.5
 parameters.targets.ldos_gridoffset_ev = -5
 parameters.hyperparameters.number_training_per_trial = 3
-parameters.running.after_training_metric = "band_energy"
+
+# Hyperparameter optimization can be further refined by using ensemble training
+# at each step and by using a different metric then the validation loss
+# (e.g. the band energy). It is recommended not to use the ensemble training
+# method in Single-GPU use, as it naturally drastically decreases performance.
+# For this small example setting, using the band energy as the after training
+# metric is not recommended, since the small data size makes
+# an accurate hyperparameter search difficult. For larger systems, enabling
+# this option is recommended.
+# parameters.running.after_training_metric = "band_energy"
 
 data_handler = mala.DataHandler(parameters)
 
