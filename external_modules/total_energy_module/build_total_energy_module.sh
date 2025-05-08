e4827d93924a37aa0def4f80fae0e3f9d1c97543
Steve Schmerler <git@elcorto.com>
2025-05-08 20:49:58 +0200

    f2py: prune temp dirs before build

--- external_modules/total_energy_module/build_total_energy_module.sh
+++ external_modules/total_energy_module/build_total_energy_module.sh
@@ -58,24 +58,27 @@ echo "qe_inc_dirs: $qe_inc_dirs"
 
 here=$(pwd)
 mod_name=total_energy
-mkdir -p $here/libs
+tmp_lib_dir=$here/libs
+meson_builddir=$here/meson_builddir
+rm -rf $tmp_lib_dir $meson_builddir
 
+mkdir -p $tmp_lib_dir
 # xc_lib.a needs to be called lib<someting>.a to be linkable with -l<something>
 cp $root_dir/XClib/xc_lib.a $here/libs/libxclib.a
 
 # Stick all object files into a static lib so that we can link them, just
 # specifying them on the f2py CLI doesn't work with the meson backend, as it
 # seems.
-ar rcs $here/libs/liballobjs.a $pwobjs $utilobjs $modobjs
+ar rcs $tmp_lib_dir/liballobjs.a $pwobjs $utilobjs $modobjs
 
 FC="$f_compiler" \
 CC="$c_compiler" \
-FFLAGS="-I$here/libs -I$pw_src_path $qe_inc_dirs" \
-LDFLAGS="-L$here/libs -L$pw_src_path $qe_lib_dirs" \
+FFLAGS="-I$tmp_lib_dir -I$pw_src_path $qe_inc_dirs" \
+LDFLAGS="-L$tmp_lib_dir -L$pw_src_path $qe_lib_dirs" \
 python3 -m numpy.f2py \
     --backend meson \
     --dep mpi \
-    --build-dir meson_builddir \
+    --build-dir $meson_builddir \
     -c ${mod_name}.f90 \
     -m $mod_name \
     $linalg \
@@ -85,7 +88,7 @@ python3 -m numpy.f2py \
 
 # Workaround for f2py+meson+mpi bug
 # (https://github.com/numpy/numpy/issues/28902)
-cd meson_builddir
+cd $meson_builddir
 sed -i -re "s/dependency\('mpi'\)/dependency\('mpi', language: 'fortran')/" meson.build
 meson setup --reconfigure bbdir
 meson compile -C bbdir
